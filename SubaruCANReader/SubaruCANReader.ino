86226862755bd736914619429dad22c73f14145a
aaroexxt <aaron.becker@gmail.com>
2019-01-30 23:28:50 -0800

    first commit

---
+++ SubaruCANReader/SubaruCANReader.ino
@@ -0,0 +1,198 @@
+//Reads all traffic on CAN0 and forwards it to CAN1 (and in the reverse direction) but modifies some frames first.
+// Required libraries
+#include "variant.h"
+#include <due_can.h>
+
+//Leave defined if you use native port, comment if using programming port
+#define Serial SerialUSB
+
+#define ECU_ID 0x07E0 //also 0x07E1
+//the above is the ECU bus ID
+
+#define ECU_RESPONSE_ID1 0x07E8
+#define ECU_RESPONSE_ID2 0x07E9
+
+int testerPresentWait = 100;
+int requestDataWait = 1000;
+
+static unsigned long testerPresentLast = 0;
+static unsigned long requestDataLast = 0;
+
+int currentState = 0;
+
+bool requestedData = false;
+
+	//********** TX REQUEST FRAME ****************
+	//where OBD PID response is CANID: BYTE_0, BYTE_1, BYTE_2, BYTE_3, BYTE_4, BYTE_5, BYTE_6, BYTE_7 
+	//
+	//                  ----------upper payload-------       -----------------lower payload----------------
+	//
+	//                 | add bytes (2) | mode | PID  |  value[0] (0x55 = NA) |  value[1]  |   value[2]  |  value[3]  |   NA  |
+	//
+	//
+	//NOTE: for transmisison requests now we are going to use the main broadcast message to speak to all ECU's 0x7DF
+	//
+	//********** RX RECEIVE FRAME ****************
+	//
+	//where OBD PID response is CANID: BYTE_0, BYTE_1, BYTE_2, BYTE_3, BYTE_4, BYTE_5, BYTE_6, BYTE_7 
+	//
+	//                  ----------upper payload-------       -----------------lower payload----------------
+	//
+	//                 | add bytes | mode & 0x40 (ack) | PID |  value[0] |  value[1]  |   value[2]  |  value[3]  |   NA  |
+	//
+	//NOTE: for now we are going to assume the main ECU response 0x7E8
+
+
+
+void sendTesterPresent()
+{
+	Serial.println("sending tester present");
+	CAN_FRAME outgoing;
+	outgoing.id = ECU_ID;
+	outgoing.extended = false;
+	outgoing.priority = 4; //0-15 lower is higher priority
+	outgoing.length = 2;
+	
+	outgoing.data.byte[0] = 0x01;
+	outgoing.data.byte[1] = 0x3E;
+	Can0.sendFrame(outgoing);
+}
+void requestEngineData()
+{
+	
+	
+	Serial.println("sending engine request");
+	CAN_FRAME outgoing;
+	outgoing.id = ECU_ID;
+	outgoing.extended = false;
+	outgoing.priority = 4; //0-15 lower is higher priority
+	outgoing.length = 8;
+	
+	outgoing.data.byte[0] = 0x02;
+	outgoing.data.byte[1] = 0x01;
+	outgoing.data.byte[2] = 0x00;
+	outgoing.data.byte[3] = 0x55;
+	outgoing.data.byte[4] = 0x55;
+	outgoing.data.byte[5] = 0x55;
+	outgoing.data.byte[6] = 0x55;
+	outgoing.data.byte[7] = 0x55;
+
+	//from this message, with some reverse engineering bytes 4-7 are what we want
+
+	Can0.sendFrame(outgoing);
+}
+
+void printFrame(CAN_FRAME &frame) {
+	Serial.print("ID: 0x");
+	Serial.print(frame.id, HEX);
+	Serial.print(" Len: ");
+	Serial.print(frame.length);
+	Serial.print(" Data: 0x");
+	for (int count = 0; count < frame.length; count++) {
+			 Serial.print(frame.data.bytes[count], HEX);
+			 Serial.print(" ");
+	}
+	Serial.print("\r\n");
+}
+
+void sendCANPacket(int ID, uint16_t data[]) {
+	CAN_FRAME outgoing;
+	outgoing.id = ID;
+	outgoing.extended = false;
+	outgoing.priority = 4;
+
+	int len = sizeof(data)/sizeof(uint16_t); //set proper length
+	outgoing.length = len;
+
+	for (int i=0; i<len; i++) {
+		outgoing.data.byte[i] = data[i];
+	}
+}
+
+bool canData() {
+}
+
+bool getCanData(int (*data)[8]) {
+	if (Can0.available() <= 0) {
+		return false;
+	}
+
+	CAN_FRAME incoming;
+	Can0.read(incoming);
+	printFrame(incoming);
+
+	int realData[8];
+	for (int count = 0; count < incoming.length; count++) {
+		realData[count] = int(incoming.data.bytes[count]); //convert to int
+	}
+	//so, there's some C++ fuckery here. basically first we have to set up a new int array with the actual bytes from CAN, converted
+
+	int (*pointerizedData)[8] = &realData; //then, we have to turn it into a pointer so the types are compatible
+	data = pointerizedData; //and then we can assign the types
+
+	return true;
+}
+
+void changeState(int newState, bool wait) {
+	Serial.print("Changing state to ");
+	Serial.println(newState);
+
+	currentState = newState;
+	
+	int data[8];
+	bool isDataPresent = getCanData(&data); //does double duty of fetching data and setting bool flag
+	if (wait && !isDataPresent) {
+		Serial.println("no response from CAN bus, waiting and trying again...");
+		delay(100);
+		changeState(newState); //call it again because hopefully we got a response
+	}
+
+
+	switch(newState) {
+		case 1:
+			sendCANPacket()
+			
+	}
+}
+
+void loop(){
+	CAN_FRAME incoming;
+
+	if (Can0.available() > 0) {
+		Serial.println("WE RECIEVED SOMETHING HOLY SHIT");
+		Can0.read(incoming);
+		printFrame(incoming);
+		 
+		if (!requestedData) {
+				requestEngineData();
+				requestedData = true;
+		}
+	}
+	
+}
+
+void setup()
+{
+
+	Serial.begin(115200);
+	
+	// Initialize CAN0 and CAN1, Set the proper baud rates here
+	Can0.begin(CAN_BPS_500K);
+
+	//setup mailboxes
+	int filter;
+	//extended
+	for (filter = 0; filter < 3; filter++) {
+	Can0.setRXFilter(filter, 0, 0, true);
+	}  
+	//standard
+	for (int filter = 3; filter < 7; filter++) {
+	Can0.setRXFilter(filter, 0, 0, false);
+	}
+
+	Serial.println("waiting to send data for 5000ms");
+	delay(5000);
+	changeState(1);
+}
+
+
